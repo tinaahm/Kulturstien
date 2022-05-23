8b1ff3343e3fe29d8bc0982ab447d0635900f09e
tinaahm <tiah001@student.kristiania.no>
2022-05-23 14:42:04 +0200

    Scrollable Map

--- Kulturstien/MainIPhoneView.swift
+++ Kulturstien/MainIPhoneView.swift
@@ -13,13 +13,35 @@ struct MainIPhoneView: View {
 	@State private var showDarkMap = false
 	@State var timeOfDayIndex = 0
 	
-	var timeOfDayImages : [[String]] = [["moon.stars.fill", "DayTimeMap"], ["sun.max.fill", "NightTimeMap"]]
+	var timeOfDayImages : [[String]] = [["moon.stars.fill", "DayTimeMapHalved"], ["sun.max.fill", "NightTimeMap"]]
 	
 	
     var body: some View {
 		ZStack {
+				
+			ZStack {
+				ButtonView(selectionType: .sawmill, image: "SawmillButton", width: 60, height: 60, posX: 200, posY: 230)
+				ButtonView(selectionType: .sawmill, image: "SawmillButton", width: 60, height: 60, posX: 70, posY: 370)
+				ButtonView(selectionType: .dam, image: "DamButton", width: 60, height: 60, posX: 110, posY: 620)
+				
+				ButtonView(selectionType: .mill, image: "MillIcon", width: 122, height: 79, posX: 350, posY: 660)
+				ButtonView(selectionType: .mill, image: "KirstiButton", width: 50, height: 65, posX: 65, posY: 65)
+				
+				ButtonView(selectionType: .none, image: "TrondButton", width: 45, height: 90, posX: 250, posY: 490)
+				ButtonView(selectionType: .none, image: "RagnhildButton", width: 45, height: 90, posX: 145, posY: 695)
+				ButtonView(selectionType: .none, image: "FlagButton", width: 35, height: 60, posX: 290, posY: 735)
+				ButtonView(selectionType: .none, image: "FlagButton", width: 35, height: 60, posX: 195, posY: 45)
+				}
+			.background(
+					Image(timeOfDayImages[timeOfDayIndex][1])
+						//.resizable()
+						//.scaledToFill()
+						.edgesIgnoringSafeArea(.all)
+				)
+			.draggable()
+			
 			VStack {
-				Group {
+				//ZStack {
 					HStack {
 						//Spacer(minLength: 0)
 						Image(systemName: "person")
@@ -49,72 +71,13 @@ struct MainIPhoneView: View {
 					.foregroundColor(.black)
 					//.frame(width: 425, height: 60)
 					//.padding()
-				}
+				//}
 				.padding()
 				.background(.white.opacity(0.5))
-				Spacer(minLength: 0)
+				//Spacer(minLength: 0)
 				
-				ZStack {/*
-					Button (action: {
-						quizSelection = .sawmill
-						page.pageIndex = .quiz
-					}) {
-						Image(systemName: "camera.macro")
-							.resizable()
-							.frame(width: 20, height: 20)
-							.padding(15)
-							.background(Color.white)
-							.foregroundColor(.black)
-						.clipShape(Circle())
-					}
-					Button (action: {}) {
-						Image(systemName: "sparkles")
-							.resizable()
-							.frame(width: 20, height: 20)
-							.padding(15)
-							.background(Color.white)
-							.foregroundColor(.black)
-						.clipShape(Circle())
-					}
-					Button (action: {}) {
-						Image(systemName: "tortoise")
-							.resizable()
-							.frame(width: 30, height: 20)
-							.padding(15)
-							.background(Color.white)
-							.foregroundColor(.black)
-						.clipShape(Circle())
-					}
-					Button (action: {}) {
-						Image(systemName: "hare")
-							.resizable()
-							.frame(width: 30, height: 20)
-							.padding(15)
-							.background(Color.white)
-							.foregroundColor(.black)
-						.clipShape(Circle())
-					}
-                    Button (action: {
-						informationSelection = .sawmill
-                        page.pageIndex = .information
-                    }) {
-                        Image("SawmillButton")
-                            .resizable()
-                            .frame(width: 60, height: 60)
-                            .padding(15)
-                    }*/
-					ButtonView(selectionType: .sawmill, image: "SawmillButton", width: 60, height: 60, posX: 200, posY: 230)
-					ButtonView(selectionType: .sawmill, image: "SawmillButton", width: 60, height: 60, posX: 70, posY: 370)
-					ButtonView(selectionType: .dam, image: "DamButton", width: 60, height: 60, posX: 110, posY: 620)
-					
-					ButtonView(selectionType: .mill, image: "MillIcon", width: 122, height: 79, posX: 350, posY: 660)
-					ButtonView(selectionType: .mill, image: "KirstiButton", width: 50, height: 65, posX: 65, posY: 65)
-					
-                    ButtonView(selectionType: .none, image: "TrondButton", width: 45, height: 90, posX: 250, posY: 490)
-					ButtonView(selectionType: .none, image: "RagnhildButton", width: 45, height: 90, posX: 145, posY: 695)
-                    ButtonView(selectionType: .none, image: "FlagButton", width: 35, height: 60, posX: 290, posY: 735)
-                    ButtonView(selectionType: .none, image: "FlagButton", width: 35, height: 60, posX: 195, posY: 45)
-				}
+	
+
 				Spacer()
 				HStack {
 					Spacer()
@@ -140,13 +103,8 @@ struct MainIPhoneView: View {
 					
 				}
 			}
+			
 		}
-		.background(
-				Image(timeOfDayImages[timeOfDayIndex][1])
-					.resizable()
-					.scaledToFill()
-					.edgesIgnoringSafeArea(.all)
-			)
 	}
 
 }
@@ -155,4 +113,24 @@ struct MainIPhoneView_Previews: PreviewProvider {
     static var previews: some View {
         MainIPhoneView().environmentObject(ViewIndex())
     }
+}
+
+struct DraggableView: ViewModifier {
+	@State var offset = CGPoint(x: 0, y: 0)
+	
+	func body(content: Content) -> some View {
+		content
+			.gesture(DragGesture(minimumDistance: 0)
+				.onChanged { value in
+					self.offset.x += value.location.x - value.startLocation.x
+					self.offset.y += value.location.y - value.startLocation.y
+			})
+			.offset(x: offset.x, y: offset.y)
+	}
+}
+
+extension View {
+	func draggable() -> some View {
+		return modifier(DraggableView())
+	}
 }
