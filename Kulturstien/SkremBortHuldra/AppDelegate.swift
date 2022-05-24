20dd52e40fdadbba9969ed767c41c5c6c96ee3cd
lucastrann <71780014+lucastrann@users.noreply.github.com>
2022-05-24 09:39:40 +0200

    lagt til huldraspill

---
+++ Kulturstien/SkremBortHuldra/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  SkremBortHuldra
+//
+//  Created by Lucas on 23/05/2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
+
