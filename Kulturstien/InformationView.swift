c0e31dc878f5fd679bff416279363e1a7eb2f239
tinaahm <tiah001@student.kristiania.no>
2022-06-08 12:50:45 +0200

    Saving quiz state

--- Kulturstien/InformationView.swift
+++ Kulturstien/InformationView.swift
@@ -13,6 +13,7 @@ struct InformationView: View {
 	@State var type: Structure
 	@State var information: [Paragraph]
 	@State var title: String
+	@State var selectedIndex: Int = 0
 	
 	init(type: Structure) {
 		self.type = type
@@ -22,7 +23,92 @@ struct InformationView: View {
     
     var body: some View {
 		
-		VStack {
+		ZStack {
+			Color("BackgroundColour")
+			
+			VStack {
+				
+				BackButtonView()
+				
+				VStack(alignment: .leading) {
+					Text(self.title)
+						.font(.title)
+				}
+				.padding(.bottom, 20)
+				
+				ZStack {
+					RoundedRectangle(cornerRadius: 25, style: .continuous)
+						.fill(.white)
+						//.shadow(radius: 4, x: -2, y: 5)
+						.frame(width: (DeviceSize.width * 0.9), height: (DeviceSize.height * 0.7))
+					
+					VStack(alignment: .center, spacing: 0) {
+						Spacer()
+						Image(self.information[self.selectedIndex].imageTitle)
+								.resizable()
+								//.scaledToFit()
+								//.aspectRatio(contentMode: .fill)
+								.frame(width: (DeviceSize.width * 0.8), height: ((DeviceSize.height * 0.7)/2.5))
+						
+						Text(self.information[self.selectedIndex].text) //FIXME: add multiline center?
+							.font(.body)
+							.foregroundColor(.black)
+							.multilineTextAlignment(.leading)
+							.frame(width: (DeviceSize.width * 0.8), height: ((DeviceSize.height * 0.7)/2))
+						Spacer()
+					}
+					
+				}
+				//.padding(.bottom, 30)
+				
+				//Spacer()
+				
+				HStack {
+					ForEach(0 ..< self.information.count) {
+						index in
+						Button (action: {
+							self.selectedIndex = index
+						}) {
+							if self.selectedIndex == index {
+								Image("")
+									.resizable()
+									.frame(width: 5, height: 5)
+									.padding(5)
+									.background(Color("DarkGrey"))
+								.clipShape(Circle())
+							} else {
+								Image("")
+									.resizable()
+									.frame(width: 1, height: 1)
+									.padding(5)
+									.background(Color("LightGrey"))
+								.clipShape(Circle())
+							}
+						}
+						.padding(1)
+					}
+				}
+			}
+		}
+		.gesture(
+			DragGesture()
+				.onEnded { value in
+					print("value", value.translation.width)
+					let direction = detectDirection(value: value)
+						  if direction == .left {
+							  if self.selectedIndex != 0 {
+								  self.selectedIndex -= 1
+								  }
+						  } else if direction == .right {
+							  if self.selectedIndex < (self.information.count - 1) {
+								  self.selectedIndex += 1
+								  }
+							  }
+						  }
+			)
+		.background(Color("BackgroundColour"))
+		
+		/*VStack {
 				BackButtonView()
 			
 			ScrollView {
@@ -74,7 +160,7 @@ struct InformationView: View {
 			}
 			.background(Color("BackgroundColour"))
 		}
-		.background(Color("BackgroundColour"))
+		.background(Color("BackgroundColour"))*/
         
     }
 }
